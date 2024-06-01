016aa8fd290ae6613e5616a4152fab9dc0970d35
AshutoshDM1 <downlodemaster1@gmail.com>
2024-06-01 22:40:18 +0530

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,21 @@ export default function Home() {
       <NavBar />
       <div className="main">
         <div className="container">
-          <h1>Home Page</h1>;
+          <div className="heading">
+            <h1>Have Your</h1>
+            <h1>Best Chat</h1>
+          </div>
+          <div className="heading_text">
+            <p>Fast, Easy and Unlimited team Chat</p>
+          </div>
+          <div className="btns">
+          <h2 className="sign_in" onClick={() => handleNavigate("/sign-in")}>
+            Sign In
+          </h2>
+          <h2 className="login" onClick={() => handleNavigate("/login")}>
+            Login
+          </h2>
+          </div>
         </div>
         <div className="container"></div>
       </div>
