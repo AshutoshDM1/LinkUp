01d6f6bfaa0613bd9502035171977b33e2c668c6
AshutoshDM1 <downlodemaster1@gmail.com>
2024-06-04 21:45:04 +0530

    done Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,51 @@
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
-import css from "../components/styles/signUp.module.css";
-import girlPic from "../assets/3d_dog_girl.png";
+import css from "../components/styles/login.module.css";
+import girlPic from "../assets/3d_man_2.png";
 import { Button, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import { login, getToken } from "../services/api.js";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = getToken();
+    if (token === null) {
+      console.log("it null");
+    } else {
+      alert("Your have been Logined")
+      navigate("/")
+    }
+  }, []);
+
+  const [input, setinput] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (event) => {
+    setinput({ ...input, [event.target.name]: event.target.value });
+  };
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+    const { username, password, confirmPassword } = input;
+    const userData = { username, password };
+    if (password !== confirmPassword) {
+      return alert("The Password Missmatch Enter again");
+    }
+    try {
+      const response = await login(userData);
+      // console.log(response)
+    } catch (error) {
+      console.error(error);
+      alert("Login failed. Please try again.");
+    }
+  };
+
   return (
     <>
       <NavBar />
@@ -13,12 +54,38 @@ const Login = () => {
           <img src={girlPic} alt="picture" />
         </div>
         <div className={css.container}>
+          <h1>Login</h1>
           <form className={css.form}>
-          <TextField className="input_box" style={{width: "90%", height: "auto"}} id="outlined-basic" label="Enter Your Name" variant="outlined" />
-          <TextField className="input_box" style={{width: "90%", height: "auto"}} id="outlined-basic" label="Enter Your Email" variant="outlined" />
-          <TextField className="input_box" style={{width: "90%", height: "auto"}} id="outlined-basic" label="Create Your Password" variant="outlined" />
-          <TextField className="input_box" style={{width: "90%", height: "auto"}} id="outlined-basic" label="Confirm Your Password" variant="outlined" />
-          <Button variant="contained">SignUp</Button>
+            <TextField
+              className="input_box"
+              name="username"
+              style={{ width: "90%", height: "auto" }}
+              id="outlined-basic"
+              label="Enter Your Name"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+              className="input_box"
+              name="password"
+              style={{ width: "90%", height: "auto" }}
+              id="outlined-basic"
+              label="Enter Your Password"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <TextField
+              className="input_box"
+              name="confirmPassword"
+              style={{ width: "90%", height: "auto" }}
+              id="outlined-basic"
+              label="Confirm Your Password"
+              variant="outlined"
+              onChange={handleChange}
+            />
+            <Button variant="contained" onClick={handleClick}>
+              Login
+            </Button>
           </form>
         </div>
       </div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,43 +1,63 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'https://auth-backend-dm.vercel.app';
+const API_URL = "https://auth-backend-dm.vercel.app";
 
 export const signUpUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/v1/user/signup/`, userData , {
-      headers : {
-        'Content-Type': 'application/json'
-      }
+    const response = await axios.post(`${API_URL}/v1/user/signup/`, userData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-    console.log('Form submitted successfully:', response.data);
-    alert('Successfully signed up!');
+    console.log("Form submitted successfully:", response.data);
+    alert("Successfully signed up!");
   } catch (error) {
     if (error.response) {
-      // The request was made and the server responded with a status code
-      console.error('Server error:', error.response.data);
+      console.error("Server error:", error.response.data);
       alert(`Server error: ${error.response.data.message}`);
     } else if (error.request) {
-      // The request was made but no response was received
-      console.error('No response from server:', error.request);
-      alert('No response from server');
+      console.error("No response from server:", error.request);
+      alert("No response from server");
     } else {
-      // Something happened in setting up the request that triggered an error
-      console.error('Request error:', error.message);
+      console.error("Request error:", error.message);
       alert(`Request error: ${error.message}`);
     }
   }
 };
 
-export const showUser = async () => {
+export const login = async (userData) => {
   try {
-    const response = await axios.get(`${API_URL}/`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    return response.data;
+    const response = await axios.post(`${API_URL}/v1/user/login` , userData, {
+      headers: 	{
+		"Content-Type": "application/json",
+	  },
+	});
+  console.log("Form submitted successfully:", response.data);
+  alert("Successfully signed up!");
+  const token =  response.data.token;
+  localStorage.setItem('token', token);
   } catch (error) {
-    console.error('Error:', error);
-    throw error;
+    if (error.response) {
+      console.error("Server error:", error.response.data);
+      alert(`Server error: ${error.response.data.message}`);
+    } else if (error.request) {
+      console.error("No response from server:", error.request);
+      alert("No response from server");
+    } else {
+      console.error("Request error:", error.message);
+      alert(`Request error: ${error.message}`);
+    }
   }
 };
+
+export const getToken = () => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return null;
+  }
+  return token;
+}
+
+export const showUser = () => {
+
+}
